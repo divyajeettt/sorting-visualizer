5338b6bedf0ccb4d6e4ec1077b963821f4b0e14c
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-26 19:25:25 +0530

    Debugging

--- src/c/quickSort.c
+++ src/c/quickSort.c
@@ -1,4 +1,9 @@
-int partition(int arr[], int start, int end)
+#include <stdlib.h>
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+int partition(int *arr, int start, int end, int *swaps, long *idx)
 {
     int temp;
     int pivot = arr[end];  // Always assume pivot as the last element
@@ -8,22 +13,28 @@ int partition(int arr[], int start, int end)
     {
         if (arr[j] < pivot)
         {
-            temp = arr[++i];
+            swaps[(*idx)++] = j;
+            swaps[(*idx)++] = ++i;
+
+            temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
     }
 
-    temp = arr[i+1];
-    arr[i+1] = arr[end];
+    swaps[(*idx)++] = ++i;
+    swaps[(*idx)++] = end;
+
+    temp = arr[i];
+    arr[i] = arr[end];
     arr[end] = temp;
 
-    return (i + 1);
+    return i;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-void sort(int arr[], int start, int end)
+void quickSort(int *arr, int start, int end, int *swaps, long *idx)
 {
     if (start >= end)
     {
@@ -32,8 +43,27 @@ void sort(int arr[], int start, int end)
     else
     {
         // arr[partitionIndex] is now at the right place
-        int partitionIndex = partition(arr, start, end);
-        sort(arr, start, partitionIndex-1);
-        sort(arr, partitionIndex+1, end);
+        int partitionIndex = partition(arr, start, end, swaps, idx);
+        quickSort(arr, start, partitionIndex-1, swaps, idx);
+        quickSort(arr, partitionIndex+1, end, swaps, idx);
     }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+int *sort(int *arr, int n)
+{
+    int *swaps = (int *) calloc(2*n*n, sizeof(int));
+    static long idx = 0;
+
+    quickSort(arr, 0, n-1, swaps, &idx);
+    printf("idx = %ld \n", idx);
+
+    printf("arr = ");
+    for (int i=0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return swaps;
 }
